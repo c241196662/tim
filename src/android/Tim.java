54c25a1976f9efe0bd54a6a6ad6299e919d12bbf
c241196662 <120004589@qq.com>
2019-03-21 17:17:21 +0800

    安卓test

--- src/android/Tim.java
+++ src/android/Tim.java
@@ -1,32 +1,240 @@
-package cordova-plugin-bakaan-tim;
+package cordova.plugin.bakaan.tim;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import com.tencent.imsdk.TIMCallBack;
+import com.tencent.imsdk.TIMConversation;
+import com.tencent.imsdk.TIMConversationType;
+import com.tencent.imsdk.TIMLogLevel;
+import com.tencent.imsdk.TIMManager;
+import com.tencent.imsdk.TIMMessage;
+import com.tencent.imsdk.TIMMessageListener;
+import com.tencent.imsdk.TIMSdkConfig;
+import com.tencent.imsdk.TIMTextElem;
+import com.tencent.imsdk.TIMValueCallBack;
+
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
+
+import cn.jpush.android.api.JPushInterface;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Tim extends CordovaPlugin {
 
+    private static final String TAG = Tim.class.getSimpleName();
+
+    public static final String ACTION_INIT = "init"; // 初始化
+    public static final String ACTION_LOGIN = "login"; // 登录
+    public static final String ACTION_LOGOUT = "logout"; // 登出
+    public static final String ACTION_SEND = "send"; // 发送
+    public static final String ACTION_ADDMESSAGELISTENER = "addmessagelistener"; // 增加消息接收监听
+    public static final String ACTION_GETHISTORYMSG = "gethistorymsg"; // 获取历史消息
+
+    public static final String ERROR_INVALID_PARAMETERS = "参数格式错误";
+
+    protected static int sdkAppId;
+
+    private Context mContext;
+    private static Activity cordovaActivity;
+
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        mContext = cordova.getActivity().getApplicationContext();
+
+        JPushInterface.init(mContext);
+
+        cordovaActivity = cordova.getActivity();
+    }
+
+    @Override
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) {
+        switch (action) {
+            case ACTION_INIT:
+                this.init(args, callbackContext);
+                return true;
+            case ACTION_LOGIN:
+                this.login(args, callbackContext);
+                return true;
+            case ACTION_LOGOUT:
+                this.logout(args, callbackContext);
+                return true;
+            case ACTION_SEND:
+                this.send(args, callbackContext);
+                return true;
+            case ACTION_ADDMESSAGELISTENER:
+                this.addMessageListener(callbackContext);
+                return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private void init(CordovaArgs args, CallbackContext callbackContext) {
+
+        final JSONObject params;
+        try {
+            params = args.getJSONObject(0);
+            sdkAppId = params.getInt("sdkAppId");
+            boolean enableLogPrint = params.has("enableLogPrint") ? params.getBoolean("enableLogPrint") : false;
+            String accountType = params.has("accountType") ? params.getString("accountType") : "0";
+            // 初始化 SDK 基本配置
+            TIMSdkConfig config = new TIMSdkConfig(sdkAppId).setAccoutType(accountType).enableLogPrint(enableLogPrint) // 是否在控制台打印Log?
+                    .setLogLevel(TIMLogLevel.DEBUG) // Log输出级别（debug级别会很多）
+                    .setLogPath(Environment.getExternalStorageDirectory().getPath() + "/timlogs/");
+            // Log文件存放在哪里？
+
+            // 初始化 SDK
+            TIMManager.getInstance().init(cordovaActivity.getApplicationContext(), config);
+        } catch (JSONException e) {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return;
         }
+    }
+
+    private void login(CordovaArgs args, CallbackContext callbackContext) {
+        final JSONObject params;
+        try {
+            params = args.getJSONObject(0);
+            String identifier = params.getString("identifier");
+            String userSig = params.getString("userSig");
+            // identifier为用户名，userSig 为用户登录凭证
+            TIMManager.getInstance().login(identifier, userSig, new TIMCallBack() {
+                @Override
+                public void onError(int code, String desc) {
+                    //错误码 code 和错误描述 desc，可用于定位请求失败原因
+                    //错误码 code 列表请参见错误码表
+                    Log.d(TAG, "login failed. code: " + code + " errmsg: " + desc);
+                }
+
+                @Override
+                public void onSuccess() {
+                    Log.d(TAG, "login succ");
+                }
+            });
+        } catch (JSONException e) {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return;
+        }
+    }
+
+    private void logout(CordovaArgs args, CallbackContext callbackContext) {
+        final JSONObject params;
+        try {
+            params = args.getJSONObject(0);
+            String identifier = params.getString("identifier");
+            String userSig = params.getString("userSig");
+            // identifier为用户名，userSig 为用户登录凭证
+            TIMManager.getInstance().logout(new TIMCallBack() {
+                @Override
+                public void onError(int code, String desc) {
+                    Log.d(TAG, "login failed. code: " + code + " errmsg: " + desc);
+                }
+
+                @Override
+                public void onSuccess() {
+                    Log.d(TAG, "login succ");
+                }
+            });
+        } catch (JSONException e) {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return;
+        }
+    }
+
+    private void send(CordovaArgs args, CallbackContext callbackContext) {
+        final JSONObject params;
+        try {
+            params = args.getJSONObject(0);
+            String msgcontent = params.getString("msg");
+            //构造一条消息并添加一个文本内容
+            TIMMessage msg = new TIMMessage();
+            TIMTextElem elem = new TIMTextElem();
+            elem.setText(msgcontent);
+            msg.addElement(elem);
+
+            TIMConversation conversation = getconversation(args);
+            //发送消息
+            conversation.sendMessage(msg, new TIMValueCallBack<TIMMessage>() {
+                @Override
+                public void onError(int code, String desc) {//发送消息失败
+                    //错误码 code 和错误描述 desc，可用于定位请求失败原因
+                    Log.d(TAG, "send message failed. code: " + code + " errmsg: " + desc);
+                }
+
+                @Override
+                public void onSuccess(TIMMessage msg) {//发送消息成功
+                    Log.e(TAG, "SendMsg ok");
+                }
+            });
+        } catch (JSONException e) {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return;
+        }
+    }
+
+
+    /**
+     * 获取回话, 读取/发送消息用
+     *
+     * @param args
+     * @return
+     */
+    private TIMConversation getconversation(CordovaArgs args) throws JSONException {
+
+        final JSONObject params;
+        try {
+            params = args.getJSONObject(0);
+            int conversationType = params.has("conversationType") ? params.getInt("conversationType") : 0;
+            TIMConversationType ctype = TIMConversationType.Invalid;
+            switch (conversationType) {
+                case 1:
+                    ctype = TIMConversationType.C2C;
+                    break;
+                case 2:
+                    ctype = TIMConversationType.Group;
+                    break;
+            }
+            //获取会话
+            String selto = params.getString("selto");//获取与用户/群组 的会话
+            TIMConversation conversation = TIMManager.getInstance().getConversation(
+                    ctype,    //会话类型：单聊/群组
+                    selto);                      //会话对方用户帐号//对方ID/群组 ID
+            return conversation;
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    private void addMessageListener(CallbackContext callbackContext) {
+        //设置消息监听器，收到新消息时，通过此监听器回调
+        TIMManager.getInstance().addMessageListener(new TIMMessageListener() {//消息监听器
+            @Override
+            public boolean onNewMessages(List<TIMMessage> msgs) {//收到新消息
+                //消息的内容解析请参考消息收发文档中的消息解析说明
+                JSONObject json = new JSONObject();
+                JSONArray msgjson = new JSONArray(msgs);
+                try {
+                    json.put("msgs", msgjson);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                callbackContext.success(json);
+                return true; //返回true将终止回调链，不再调用下一个新消息监听器
+            }
+        });
     }
 }
--- www/Tim.js
+++ www/Tim.js
@@ -1,5 +1,89 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Tim', 'coolMethod', [arg0]);
+
+module.exports = {
+    /**
+     * 初始化腾讯云通信
+     *
+     * @example
+     * <code>
+     * Tim.init({
+     *     sdkAppId: 0, //从云通信控制台创建应用获取到的sdkappid
+     *     accountType: 0, //用户类型,自建还是依托于腾讯
+     *     enableLogPrint: true // 是否开启打印日志,默认为false
+     * }, function () {
+     *     alert("Success");
+     * }, function (reason) {
+     *     alert("Failed: " + reason);
+     * });
+     * </code>
+     */
+    init: function (message, onSuccess, onError) {
+        exec(onSuccess, onError, "Tim", "init", [message]);
+    },
+    /**
+     * 登录
+     *
+     * @example
+     * <code>
+     * Tim.login({
+     *     identifier: 0, // 用户名
+     *     userSig: 0 // 用户登录凭证
+     * }, function () {
+     *     alert("Success");
+     * }, function (reason) {
+     *     alert("Failed: " + reason);
+     * });
+     * </code>
+     */
+    login: function (message, onSuccess, onError) {
+        exec(onSuccess, onError, "Tim", "login", [message]);
+    },
+    /**
+     * 登出
+     *
+     * @example
+     * <code>
+     * Tim.logout({}, function () {
+     *     alert("Success");
+     * }, function (reason) {
+     *     alert("Failed: " + reason);
+     * });
+     * </code>
+     */
+    logout: function (message, onSuccess, onError) {
+        exec(onSuccess, onError, "Tim", "logout", [message]);
+    },
+    /**
+     * 发送消息
+     *
+     * @example
+     * <code>
+     * Tim.send({
+     *     msg: 'a bew test msg' // 文本内容
+     * }, function (msg) {
+     *     alert("msg: " + msg);
+     * }, function (reason) {
+     *     alert("Failed: " + reason);
+     * });
+     * </code>
+     */
+    send: function (message, onSuccess, onError) {
+        exec(onSuccess, onError, "Tim", "send", [message]);
+    },
+    /**
+     * 接收消息
+     *
+     * @example
+     * <code>
+     * Tim.addmessagelistener({}, function (msgs) {
+     *     alert("msgs: " + msgs);
+     * }, function (reason) {
+     *     alert("Failed: " + reason);
+     * });
+     * </code>
+     */
+    addmessagelistener: function (message, onSuccess, onError) {
+        exec(onSuccess, onError, "Tim", "addmessagelistener", [message]);
+    }
 };
